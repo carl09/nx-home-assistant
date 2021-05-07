0e93a8d51a1e305504a3c4c1fdb2ed2e1d459554
Carl <carl.hickton@nintex.com>
2021-05-07 23:12:23 +1000

    asd

--- local_streamer/rootfs/usr/bin/example2.sh
+++ local_streamer/rootfs/usr/bin/example2.sh
@@ -22,7 +22,7 @@ main() {
         bashio::log.info "$i" | jq '.url'
         bashio::log.info "++"
 
-        endpoint="$i" | jq -r '.[0]'
+        endpoint="$i" | jq '.endpoint'
         bashio::log.info $endpoint
         bashio::log.info ""
 

0397cc7ace9b6c9053a1d1b13bd770e2a0eda0f1
Carl <carl.hickton@nintex.com>
2021-05-07 21:55:27 +1000

    sdf

--- local_streamer/startup.sh
+++ local_streamer/startup.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env bashio
 
 # INPUT=$PARAMETERS
 
@@ -26,9 +26,14 @@ bashio: : log.trace "Hello"
 streams =$(bashio: : config 'streams')
 bashio:: log.info "Seconds between each quotes is set to: ${streams}"
 
-bashio: : log.trace "Starting ${OUT} stream"
+bashio: : log.info "Starting ${OUT} stream"
 
-$(sh ./create_ffmpeg_cmd.sh "rtsp://192.168.10.74:554/h264" help) &
+for i in streams
+do
+	bashio:: log.info $i
+done
+
+# $(sh ./create_ffmpeg_cmd.sh "rtsp://192.168.10.74:554/h264" help) &
 
 
 # ffmpeg -rtsp_transport tcp -i rtsp://192.168.10.74:554/h264 -f lavfi -i aevalsrc=0 -acodec aac -vcodec copy -hls_list_size 2 -hls_init_time 1 -hls_time 1 -hls_flags delete_segments /tmp/stream/help.m3u8

8ac267e8a2e045027744f2177844cabe437f5ff4
Carl <carl.hickton@nintex.com>
2021-05-07 22:29:04 +1000

    updatin

--- local_streamer/rootfs/usr/bin/example2.sh
+++ local_streamer/rootfs/usr/bin/example2.sh
@@ -5,11 +5,23 @@
 # Example add-on for Home Assistant.
 # ------------------------------------------------------------------------------
 main() {
-    bashio::log.trace "${FUNCNAME[0]}"
+    bashio: : log.trace "Hello"
 
-    while true; do
-        echo "Second Script Output"
-        sleep 10
+    streams =$(bashio: : config 'streams')
+    bashio:: log.info "Seconds between each quotes is set to: ${streams}"
+
+    bashio: : log.info "Starting ${OUT} stream"
+
+    for i in streams
+    do
+        bashio:: log.info $i
     done
+
+    # $(sh ./create_ffmpeg_cmd.sh "rtsp://192.168.10.74:554/h264" help) &
+
+
+    # ffmpeg -rtsp_transport tcp -i rtsp://192.168.10.74:554/h264 -f lavfi -i aevalsrc=0 -acodec aac -vcodec copy -hls_list_size 2 -hls_init_time 1 -hls_time 1 -hls_flags delete_segments /tmp/stream/help.m3u8
+
+    nginx -g "daemon off;"
 }
 main "$@"
